17733954d4bc7c0fb97ce7bedd9cb148ad465570
moa3337 <moa-x@hotmail.com>
2023-01-23 15:16:15 +0100

    completamento seconda parte

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
+// calcolare prezzo biglietto in base ai km,
+// con sconti del 20% o 40% in base all'età
+// il prezzo è di 0,21 centesimi al km
+
+console.log('prezzo bigliett 0.21€/Km');
+
 // km DA PERCORRERE
-let km = prompt('quanti km vuoi percorrere?')
+const km = prompt('quanti km vuoi percorrere?')
 
 // ETA' PASSEGERO
-let età = prompt('quanti anni hai?')
+const age = prompt('quanti anni hai?')
 
+// PREZZO EURO/km
+let price = km * 0.21
+console.log(price + '€');
+
+// SCONTO 20% UNDER 18
+
+// SCONTO 40% OVER 65
